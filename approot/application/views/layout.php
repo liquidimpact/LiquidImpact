e1c4119f612a13499ec8c7f2ef7e951026268b06
freefcw <freefcw@gmail.com>
2012-03-20 22:48:49 +0800

    default layout

---
+++ approot/application/views/layout.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title><?php echo $title;?></title>
+</head>
+<body>
+<ul class="nav nav-tabs">
+  <li class="dropdown">
+    <a class="dropdown-toggle"
+       data-toggle="dropdown"
+       href="#">
+        Dropdown
+        <b class="caret"></b>
+      </a>
+    <ul class="dropdown-menu">
+      <!-- links -->
+    </ul>
+  </li>
+</ul>
+<div class="container">
+  <?php echo $body;?>
+</div>
+
+</body>
+</html>

c33898fe7d3b5ca5e434399aff9b5c91af697bb5
freefcw <freefcw@gmail.com>
2012-03-20 17:30:02 +0800

    model_base

---
+++ approot/application/classes/model/base.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Base extends Model_Database {
+   public function query($sql)
+   {
+       return $this->_db->query(Database::SELECT, $sql);
+   }
+
+    public function delete($sql)
+    {
+        return $this->_db->query(Database::DELETE, $sql);
+    }
+
+    public function update($sql)
+    {
+        return $this->_db->query(Database::UPDATE, $sql);
+
+    }
+
+    public function insert($sql)
+    {
+        return $this->_db->query(Database::INSERT, $sql);
+    }
+}

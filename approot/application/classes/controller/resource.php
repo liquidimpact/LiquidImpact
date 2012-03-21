d28d4aa39750f072a8230deb9c0e6826ab26db37
freefcw <freefcw@gmail.com>
2012-03-21 19:37:25 +0800

    basic resource controller

--- approot/application/classes/controller/resource.php
+++ approot/application/classes/controller/resource.php
@@ -2,25 +2,48 @@
 
 class Controller_Resource extends Controller_Base {
 
-	public function action_index()
-	{
-		$this->response->body('hello, world!');
-	}
-
     public function action_list()
-    {}
-
-    public function action_edit()
     {
+        $page = $this->request->param('page', 1);
 
+        $m = new Model_Resource();
+        $rlist = $m->get_page($page);
+
+        $body = View::factory('resource/list');
+        $body->bind_global('rlist', $rlist);
+
+        $body->set_global('title', 'Resource List '. $page);
+        $this->body = $body;
     }
 
     public function action_del()
     {
+        $rid = $this->request->param('rid', null);
+        if ($rid === null) $this->request->redirect('/');
 
+        $m = new Model_Resource();
+        $fileinfo = $m->get_resource($rid);
+
+        $m->rm($rid);
+
+        unlink($fileinfo['fpath']);
+
+        $this->request->redirect('/resource');
     }
 
-    public function action_new()
-    {}
+    public function action_upload()
+    {
+        if ($this->request->method() === 'POST')
+        {
+            Upload::save($_FILES['userfile']);
+            $m = new Model_Resource();
 
+            $m->add($data);
+        }
+    }
+
+    public function action_index()
+    {
+        $this->action_list();
+    }
 } // End Welcome
---
+++ approot/application/classes/model/resource.php
@@ -0,0 +1,43 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Resource extends Model_Base {
+
+    public function get_resource($rid)
+    {
+        $sql = "SELECT *
+                FORM resources WHERE `resource_id` == {$rid}";
+
+        $result = $this->query($sql);
+        return $result->current();
+    }
+
+    public function rm($rid)
+    {
+        $sql = "DELETE * FROM resources WHERE `resouce_id` = {$rid}";
+
+        return $this->delete($sql);
+    }
+
+    public function add($data)
+    {
+        $sql = "INSERT INTO resource(`fpath`)
+                values ({$data['fpath']})
+                ";
+        list($id, $row) = $this->insert($sql);
+        return $id;
+    }
+
+    public function get_page($page, $per_page = 20)
+    {
+        $offset = ($page - 1) * $per_page;
+        $sql = "SELECT *
+                FROM resources
+                ORDER BY
+                  `resource_id` DESC
+                LIMIT {$per_page}, {$offset};
+                ";
+
+        $result = $this->query($sql);
+        return $result->as_array();
+    }
+}

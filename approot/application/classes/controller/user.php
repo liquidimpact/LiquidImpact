607341845abf6c9129cab294da0011a59384e18a
freefcw <freefcw@gmail.com>
2012-03-21 11:54:02 +0800

    user controller

--- approot/application/classes/controller/user.php
+++ approot/application/classes/controller/user.php
@@ -18,6 +18,8 @@ class Controller_User extends Controller_Base {
     {
         $page = $this->request->param('page', 1);
 
+        $m = new Model_User();
+
     }
 
     public function action_edit()

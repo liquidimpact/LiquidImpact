e9c0139070885e496a72861f1f5f156664c4d3a2
freefcw <freefcw@gmail.com>
2012-03-22 22:32:21 +0800

    add permission

---
+++ approot/application/classes/he.php
@@ -0,0 +1,18 @@
+<?php
+
+class He {
+    static $category = array(
+        0 => 'Please Choose',
+        1 => 'Live Public',
+        2 => 'Gala',
+        3 => 'Fun & Play',
+        4 => 'Corporate',
+        5 => 'Life Style'
+    );
+
+    static $permission = array(
+        0 => 'Please Choose',
+        1 => 'Administrator',
+        2 => 'Upload Resource Only',
+    );
+}

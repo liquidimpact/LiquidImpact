af448b90f62d152ef554feb9307ec12291bcc8ef
freefcw <freefcw@gmail.com>
2012-03-23 15:05:33 +0800

    add generate_fields

--- approot/application/classes/model/base.php
+++ approot/application/classes/model/base.php
@@ -21,4 +21,14 @@ class Model_Base extends Model_Database {
     {
         return $this->_db->query(Database::INSERT, $sql);
     }
+
+    protected function generate_field($data)
+    {
+        $arr = array();
+        foreach($data as $key=>$val)
+        {
+            array_push($arr, "`{$key}` = '{$val}'");
+        }
+        return implode(',', $arr);
+    }
 }

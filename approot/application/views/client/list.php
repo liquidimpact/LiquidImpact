5839a5b0c58657defa2a2d6e022d9b8f5beb6787
freefcw <freefcw@gmail.com>
2012-03-23 16:43:43 +0800

    add logo, add edit link

--- approot/application/views/client/list.php
+++ approot/application/views/client/list.php
@@ -4,14 +4,16 @@
         <td>#</td>
         <td>Name</td>
         <td>Description</td>
+        <td>Logo</td>
     </tr>
     </thead>
     <tbody>
     <?php foreach($client_list as $client):?>
         <tr>
     <td><?php echo $client['client_id'];?></td>
-    <td><?php echo $client['name'];?></td>
+    <td><a href="/client/edit/<?php echo $client['client_id'];?>"><?php echo $client['name'];?></a></td>
     <td><?php echo $client['description'];?></td>
+    <td><img src="<?php echo $client['logo'];?>" alt="logo"/></td>
         </tr>
     <?php endforeach;?>
     </tbody>

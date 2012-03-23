1e29b17f81268d367f73345c7a5df18878ad2ea3
freefcw <freefcw@gmail.com>
2012-03-23 14:24:51 +0800

    add database config

---
+++ approot/application/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array
+(
+    'default' => array
+    (
+        'type'       => 'mysql',
+        'connection' => array(
+            'hostname'   => 'localhost',
+            'username'   => 'root',
+            'password'   => 'hejun',
+            'persistent' => FALSE,
+            'database'   => 'liquid',
+        ),
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'profiling'    => TRUE,
+    ),
+);
